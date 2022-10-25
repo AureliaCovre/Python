83dc53bc6c3af87abea47883396f636852009fab
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-10-25 10:47:41 -0300

    Create aula.py

---
+++ Conhecendo_Tuplas_em_Python/aula.py
@@ -0,0 +1,18 @@
+### CURSO: CONHECENDO TUPLAS EM PYTHON 
+# Link: https://web.dio.me/course/conhecendo-tuplas-em-python/learning/5ce0d4ad-7182-4c6c-b798-076ffa6fc958?back=/track/geracao-tech-unimed-bh-ciencia-de-dados&tab=undefined&moduleId=undefined
+# Instrutor: Guilherme Arthur de Carvalho
+
+""" Objetivo geral: Entender o funcionamento da estrutura de dados tuplas.
+    Criando tuplas: Tuplas são estruturas de dados muito parecidas com as listas, a principla 
+diferença é que tuplas são imutáveis enquanto listas são mutáveis. Podemos criar tuplas através 
+da classe tuple, ou colocando valores separados por vírgula de parenteses.  """
+
+frutas = ("laranja", "pera", "uva",)
+
+letras = tuple("python")
+
+numeros = tuple([1,2,3,4])
+
+pais = ("Brasil",)
+
+frutas = ("maçã", "laranja", "uva", "")
