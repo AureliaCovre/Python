4acae65b1c547fb7bd15e8cdeea327dead5a356b
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-09 10:45:45 -0300

    Create fundamento_etl.py

---
+++ PYTHON/fundamento_etl.py
@@ -0,0 +1,39 @@
+### CURSO: FUNDAMENTOS DE ETL (EXTRACT, TRANSFORM, LOAD) COM PYTHON
+# Link: https://web.dio.me/course/fundamentos-de-etl-extract-transform-load-com-python/learning/6413a71f-938e-4366-8845-4a70c84e9114?back=/track/geracao-tech-unimed-bh-ciencia-de-dados&tab=undefined&moduleId=undefined
+# Instrutor: Diego Renan
+
+"""  ETL 
+Ferramentas utilizadas: As ferramentas são softwares utilizados para facilitar o processo de 
+integração de dados. Inicialmente muito usados em projetos de Data Warehouse e Business Intelligence em
+geral, ultimamente tem sido utilizados em processos de integração de software, banco de dados, etc.
+
+Existem diversas ferramentas de ETL, como:
+- IBM Data Stage
+- Power Center
+- Sql Server Integration Services
+- Talend ETL
+
+ETL para BIG DATA - Projetos Hadoop:
+- Sqoop: Ferramenta para movimentar dados dentre bancos de dados relacionais e o ambiente Haddop
+- Hive: Ambiente de SQL sobre um cluster Haddop
+- PIG: Ferramenta de Script para transformação e processamento de dados. 
+- SPARK: Framework de processamento em memória.
+"""
+
+""" Introdução a biblioteca Pandas """
+# df.shape  #Retorna qtd de linhas e colunas
+# df.info() #Para saber o formato dos dados de cada coluna e qtd de memória para ler esse conj de dados.
+# df.isnull().sum() #Verifica quantos dados faltantes existem em nosso conjunto
+# df["setor"].unique() #Verifica quais os valores únicos  
+# df["setor"].values_counts().plot(kind='bar') #Visualização simples e rápida utilizando o método plot (biblioteca matplotlib)
+# df.describe() #Dados estatísticos
+
+
+""" Introdução a biblioteca Scikit-learn """
+""" Esta biblioteca dispõe de ferramentas simples e eficientes para análise preditiva de dados, é 
+reutilizável em diferentes situações, possui código aberto, sendo acessível a todos e foi construída
+sobre os pacotes NumPy, SciPy e matplotilib. """
+
+
+
+
